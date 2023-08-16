827e3a5d872b79a3d367844daba6ea9889c9716d
yakhyo <yakhyo9696@gmail.com>
2023-08-16 18:17:52 +0900

    feat: add the test data

--- main.py
+++ main.py
@@ -14,6 +14,22 @@ from utils.misc import strip_optimizers, add_weight_decay
 
 from utils import LOGGER
 
+def jaccard_index(pred, target, num_classes):
+    # Convert the prediction and target tensors to one-hot encoding
+    pred = torch.softmax(pred, dim=1)
+    pred = torch.argmax(pred, dim=1)
+    pred = torch.nn.functional.one_hot(pred, num_classes=num_classes)
+    target = torch.nn.functional.one_hot(target, num_classes=num_classes)
+
+    # Calculate the intersection and union tensors
+    intersection = (pred & target).float().sum((0, 1, 2))
+    union = (pred | target).float().sum((0, 1, 2))
+
+    # Calculate the Jaccard Index for each class
+    jaccard = intersection / (union + 1e-15)
+
+    return jaccard
+
 
 def train(opt, model, device):
     best_score, start_epoch = 0, 0
@@ -48,22 +64,24 @@ def train(opt, model, device):
         del ckpt
 
     # Dataset
-    dataset = DamageDataset(root="./data", image_size=opt.image_size, mask_suffix="")
+    trainval_dataset = DamageDataset(root="./data/train", image_size=opt.image_size, mask_suffix="")
+    test_dataset = DamageDataset(root="./data/test", image_size=opt.image_size, mask_suffix="")
 
     # Split
-    n_val = int(len(dataset) * 0.1)
-    n_train = len(dataset) - n_val
-    train_data, test_data = random_split(dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0))
+    n_val = int(len(trainval_dataset) * 0.1)
+    n_train = len(trainval_dataset) - n_val
+    train_data, val_data = random_split(trainval_dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0))
 
     # DataLoader
     train_loader = DataLoader(train_data, batch_size=opt.batch_size, num_workers=8, shuffle=True, pin_memory=True)
-    test_loader = DataLoader(test_data, batch_size=opt.batch_size, num_workers=8, drop_last=True, pin_memory=True)
+    val_loader = DataLoader(val_data, batch_size=opt.batch_size, num_workers=8, drop_last=False, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=opt.batch_size, num_workers=8, drop_last=False, pin_memory=True)
 
     # Training
     for epoch in range(start_epoch, opt.epochs):
         model.train()
         epoch_loss = 0
-        LOGGER.info(("\n" + "%12s" * 5) % ("Epoch", "GPU Mem", "CE Loss", "Dice Loss", "Total Loss"))
+        LOGGER.info(("\n" + "%12s" * 6) % ("Epoch", "GPU Mem", "CE Loss", "Dice Loss", "Total Loss", "iou"))
         progress_bar = tqdm(train_loader, total=len(train_loader))
         for image, target in progress_bar:
             image = image.to(device)
@@ -72,6 +90,7 @@ def train(opt, model, device):
             with torch.cuda.amp.autocast(enabled=opt.amp):
                 output = model(image)
                 loss, losses = criterion(output, target)
+                iou = jaccard_index(output, target, num_classes=7)
 
             optimizer.zero_grad(set_to_none=True)
             grad_scaler.scale(loss).backward()
@@ -79,13 +98,16 @@ def train(opt, model, device):
             grad_scaler.update()
 
             epoch_loss += loss.item()
+            iou = float(iou.mean())
             mem = f"{torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0:.3g}G"  # (GB)
             progress_bar.set_description(
-                ("%12s" * 2 + "%12.4g" * 3) % (f"{epoch + 1}/{opt.epochs}", mem, losses["ce"], losses["dice"], loss)
+                ("%12s" * 2 + "%12.4g" * 4) % (f"{epoch + 1}/{opt.epochs}", mem, losses["ce"], losses["dice"], loss,iou)
             )
 
-        dice_score, dice_loss = validate(model, test_loader, device)
-        LOGGER.info(f"VALIDATION: Dice Score: {dice_score:.4f}, Dice Loss: {dice_loss:.4f}")
+        dice_score, dice_loss, miou = validate(model, val_loader, device)
+        LOGGER.info(f"VALIDATION: Dice Score: {dice_score:.4f}, Dice Loss: {dice_loss:.4f}, mIOU: {miou}")
+        dice_score, dice_loss, miou = validate(model, test_loader, device)
+        LOGGER.info(f"TEST: Dice Score: {dice_score:.4f}, Dice Loss: {dice_loss:.4f}, mIOU: {miou}")
         scheduler.step(epoch)
         ckpt = {
             "epoch": epoch,
@@ -110,6 +132,7 @@ def validate(model, dataloader, device, conf_threshold=0.5):
     model.eval()
     dice_score = 0
     criterion = DiceLoss()
+    ious = []
     for image, target in tqdm(dataloader, total=len(dataloader)):
         image, target = image.to(device), target.to(device)
         with torch.no_grad():
@@ -117,17 +140,19 @@ def validate(model, dataloader, device, conf_threshold=0.5):
             if model.out_channels == 1:
                 output = F.sigmoid(output) > conf_threshold
             dice_loss = criterion(output, target)
+            iou = jaccard_index(output, target, num_classes=7)
+            ious.append(float(iou.mean()))
             dice_score += 1 - dice_loss
     model.train()
 
-    return dice_score / len(dataloader), dice_loss
+    return dice_score / len(dataloader), dice_loss, sum(ious)/len(ious)
 
 
 def parse_opt():
     parser = argparse.ArgumentParser(description="UNet training arguments")
     parser.add_argument("--image_size", type=int, default=512, help="Input image size, default: 512")
     parser.add_argument("--save-dir", type=str, default="weights", help="Directory to save weights")
-    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs, default: 5")
+    parser.add_argument("--epochs", type=int, default=50, help="Number of epochs, default: 5")
     parser.add_argument("--batch-size", type=int, default=2, help="Batch size, default: 12")
     parser.add_argument("--loss", type=str, default="dice", help="Loss function, available: dice, dice_ce, focal")
     parser.add_argument("--lr", type=float, default=1e-5, help="Learning rate, default: 1e-5")
